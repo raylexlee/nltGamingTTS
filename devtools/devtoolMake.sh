37241203d7c5eabb0dbdc902d60613fe691cddc5
Raylex Lee <raylexlee@outlook.com>
2026-09-11 00:58:45 +0800

    Fix devtoolsMake

--- devtools/devtoolMake.sh
+++ devtools/devtoolMake.sh
@@ -2,6 +2,5 @@
 TAG=${1?'missing name tag, eg epidemic,nadia,order,epidemic'}
 JSON="$TAG"VOICEwin.json
 SCRIPT="$TAG".js
-cp 1stHALFjs.txt $SCRIPT
-cat $JSON >> $SCRIPT
-cat 2ndHALFjs.txt >> $SCRIPT
+cp base.js $SCRIPT
+vim -O $SCRIPT $JSON
